df06c2e612aefa952555da8ead7f01e764f161b0
Andrew Savelev <savelevcorr@gmail.com>
2015-10-19 13:33:37 +1000

    Update app.js
    Remove unusable variables

--- src/js/app.js
+++ src/js/app.js
@@ -17,15 +17,11 @@ Modernizr.load({
                 autoclose: true
             });
 
-            var cityId;
-
             $('#city').kladr({
                 token: '5623b69e0a69de320f8b45fa',
                 type: $.kladr.type.city,
 
                 select: function( obj ){
-                    cityId = obj.id;
-
                     $('#street').kladr({
                         parentId: obj.id,
                         type: $.kladr.type.street,
